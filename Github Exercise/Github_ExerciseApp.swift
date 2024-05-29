137f740c33280d61b9b921616a904c8d728ea682
lostmachine18 <lostmachine@ukr.net>
2024-05-29 23:41:56 +0300

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by De Stroy on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
